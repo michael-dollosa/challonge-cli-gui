0d4fbcb8300ce8a34a8f86e461476ed23d0f2a12
Michael Dollosa <michaeldollosa@dolees-Macbook-Pro.local>
2021-07-30 19:23:05 +0800

    added delete in hint options for auto complete

--- src/helper/hintOptions.js
+++ src/helper/hintOptions.js
@@ -1,14 +1,14 @@
 export const options = [
   "@tournament",
   "@tournament -a",
-  "@tournament -s",
   "@tournament -s \"Tournament URL\"",
+  "@tournament -d \"Tournament URL\"",
   "@match",
   "@match -a \"Tournament URL\"",
-  "@match -s  \"Tournament URL\" \"Match URL\"",
+  "@match -s \"Tournament URL\" \"Match URL\"",
   "@api",
   "@api -g",
-  "@api -s  \"API Key\"",
+  "@api -s \"API Key\"",
   "@commands",
 
 ]
